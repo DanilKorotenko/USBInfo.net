7e30d70b1b995012e75e3d4f87a79c50a145d6d4
dkorotenko_pc\dkorotenko <dkorotenko@adps.dpsu>
2025-04-02 11:22:25 +0300

    Print properties function added.

--- USBInfo/USBInfo.cs
+++ USBInfo/USBInfo.cs
@@ -7,7 +7,45 @@ namespace USBInfo;
 public class USBInfo
 {
 
-   public static string getSerialNumber()
+    public static void printProperties()
+    {
+        try
+        {
+            // Create a ManagementObjectSearcher to query USB devices
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"SELECT * FROM Win32_USBControllerDevice");
+
+            foreach (ManagementObject usbDevice in searcher.Get())
+            {
+                // Get the associated PnPDeviceID
+                string deviceId = usbDevice["Dependent"].ToString() ?? "";
+                if (deviceId.Length == 0)
+                {
+                    break;
+                }
+                deviceId = deviceId.Split('=')[1].Trim('"');
+
+                // Query the PnPDevice for the serial number
+                ManagementObjectSearcher deviceSearcher = new ManagementObjectSearcher($"SELECT * FROM Win32_PnPEntity WHERE DeviceID='{deviceId}'");
+                foreach (ManagementObject device in deviceSearcher.Get())
+                {
+                    if (device["Service"].ToString() == "USBSTOR")
+                    {
+                        foreach (PropertyData prop in device.Properties)
+                        {
+                            Console.WriteLine("{0}: {1}", prop.Name, prop.Value);
+                        }
+                        Console.WriteLine("***************************");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }        
+    }
+
+    public static string getSerialNumber()
     {
         try
         {
@@ -48,21 +86,7 @@ public class USBInfo
                         {
                             Console.WriteLine($"An error occurred: {ex.Message}");
                         }
-                        // foreach (PropertyData prop in device.Properties)
-                        // {
-                        //     Console.WriteLine("{0}: {1}", prop.Name, prop.Value);
-                        // }
-                        // Console.WriteLine("***************************");
                     }
-                    // try
-                    // {
-                    //     Console.WriteLine($"Device: {device["Name"]}");
-                    //     Console.WriteLine($"Serial Number: {device["SerialNumber"]}");
-                    // }
-                    // catch(Exception ex)
-                    // {
-                    //     Console.WriteLine($"An error occurred: {ex.Message}");
-                    // }
                 }
             }
         }
@@ -70,7 +94,6 @@ public class USBInfo
         {
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
-
         return "No serial number found";
     }
 
